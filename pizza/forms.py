768abdc089a3c0afb6086dc98c4c74605819fd11
adenbagja <adenbagja46@gmail.com>
2022-04-19 11:47:35 +0700

    add more forms

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,16 +1,21 @@
 # add
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 #membuat form pizza, agar form.html bisa dibuat template
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length=100)
-    topping2 = forms.CharField(label='Topping 2', max_length=100)
-    size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', "Medium"), ('Large', 'Large')])
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', "Medium"), ('Large', 'Large')])
 
-#make form pizza from models.py
-# class PizzaForm(forms.ModelForm):
-#     class Meta:
-#         model = Pizza
-#         fields = ['topping1', 'topping2', 'size']
-#         labels = {'topping1':'Topping 1', 'topping2':'Topping 2'}
+# make form pizza from models.py
+class PizzaForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1':'Topping 1', 'topping2':'Topping 2'}
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
